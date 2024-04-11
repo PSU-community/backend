f693a2c5cf5698b5059420b6f400b69ba31193b0
Damego <danyabatueff@gmail.com>
2024-04-11 19:27:23 +0500

    refactor: add get_many method & fix typing

--- src/utils/repository.py
+++ src/utils/repository.py
@@ -18,6 +18,10 @@ class Repository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    async def get_many(self, **filter: Any):
+        raise NotImplementedError
+
+    @abstractmethod
     async def add_one(self, data: dict):
         raise NotImplementedError
 
@@ -36,13 +40,18 @@ class SQLAlchemyRepository(Repository):
     async def get_by_id(self, id: int):
         return await self.get_one(id=id)
 
-    async def get_one(self, **filter: Unpack[table_model]) -> UserSchema | None:
+    async def get_one(self, **filter: Unpack[table_model]) -> table_model | None:
         async with async_session_maker() as session:
             result = await session.execute(select(self.table_model).filter_by(**filter))
             data = result.scalar_one_or_none()
             return data.to_schema_model() if data else None
 
-    async def add_one(self, data: dict) -> UserSchema:
+    async def get_many(self, **filter: Unpack[table_model]) -> list[table_model]:
+        async with async_session_maker() as session:
+            result = await session.execute(select(self.table_model).filter_by(**filter))
+            return [table.to_schema_model() for table in result.scalars().all()]
+
+    async def add_one(self, data: dict) -> table_model:
         async with async_session_maker() as session:
             result = await session.execute(
                 insert(self.table_model).values(**data).returning(self.table_model)
@@ -54,7 +63,7 @@ class SQLAlchemyRepository(Repository):
         async with async_session_maker() as session:
             await session.execute(delete(self.table_model).filter_by(id=id))
 
-    async def find_all(self) -> list[UserSchema]:
+    async def find_all(self) -> list[table_model]:
         async with async_session_maker() as session:
             result = await session.execute(
                 select(self.table_model)
