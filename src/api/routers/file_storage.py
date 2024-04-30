a075b5137d6ab9d500f4fe2ba52c6e5812855b3d
Damego <damego.dev@gmail.com>
2024-04-30 11:17:32 +0500

    feat!: request for admin for file upload

--- src/api/routers/file_storage.py
+++ src/api/routers/file_storage.py
@@ -2,6 +2,7 @@ from fastapi import Depends, UploadFile, APIRouter
 from fastapi.responses import FileResponse
 from fastapi.security import HTTPBearer
 
+from src.api.dependencies import IAdminUser
 from src.repositories.local_file_storage_repository import LocalFileStorageRepository
 
 router = APIRouter(dependencies=[Depends(HTTPBearer(auto_error=False))])
@@ -12,8 +13,9 @@ router = APIRouter(dependencies=[Depends(HTTPBearer(auto_error=False))])
 
 # TODO: add admin user depends
 @router.post("/upload")
-async def upload(file: UploadFile):
+async def upload(file: UploadFile, user: IAdminUser):
     name = await LocalFileStorageRepository.upload_file(file)
+
     return {"url": f"http://127.0.0.1:8000/file/{name}"}
 
 
