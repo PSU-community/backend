96e62cd1e58aa3d556912df59b284af522d2b8fb
Damego <danyabatueff@gmail.com>
2024-05-23 13:58:52 +0500

    fix: load subcategories contents in post

--- src/repositories/content_repository.py
+++ src/repositories/content_repository.py
@@ -169,4 +169,4 @@ class ContentRepository:
             )
 
             result = await session.execute(query)
-            return [table.to_schema_model(load_subcategories=True, load_post=True) for table in result.scalars().all()]
+            return [table.to_schema_model(load_subcategories=True, load_subcategories_posts=True, load_post=True) for table in result.scalars().all()]
